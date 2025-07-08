5c230fa457954481b08e0e0495a3359c71a2e02f
Sanchai Ahilan <sanchai.ahilan@indium.tech>
2025-07-08 09:35:01 +0530

    Initial Commit

---
+++ TruVideo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TruVideo
+//
+//  Created by Sanchai Ahilan J K  on 08/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TruVideo/TruVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TruVideoApp.swift
+//  TruVideo
+//
+//  Created by Sanchai Ahilan J K  on 08/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TruVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
